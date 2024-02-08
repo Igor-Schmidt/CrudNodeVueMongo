254b26b00dd9cde0ff4e82ca9a85543c3f537ae0
IZS <igorschmidt52@gmail.com>
2024-02-07 23:12:35 -0300

    Alteracoes front

--- frontend/src/components/create-employee/CreateNewUser.vue
+++ frontend/src/components/create-employee/CreateNewUser.vue
@@ -5,25 +5,44 @@
           <h3>New User</h3>
         </div>
         <div class="card-body">
-          <form v-on:submit.prevent="">
+          <form v-on:submit.prevent="handleSubmitForm">
             <!-- INICIO DE BLOCO: New User - Name -->
             <div class="form-group">
-              <label class="font-weight-bold">User Name</label>
-              <input type="text" class="form-control" placeholder="Type your Name">
+              <label for="userFormName" class="font-weight-bold">User Name <input type="text"
+              id="userFormName"></label>
+              <!-- <input type="text"
+                id="userFormName"
+                class="form-control"
+                placeholder="Type your Name"
+                v-model="components.name"
+                required
+              > -->
             </div>
             <!-- FIM DE BLOCO: New User - Name -->
 
             <!-- INICIO DE BLOCO: New User - Email -->
             <div class="form-group">
-              <label class="font-weight-bold">User Email</label>
-              <input type="text" class="form-control" placeholder="Type your Email@">
+              <!-- <label htmlFor="userFormEmail" class="font-weight-bold">User Email</label> -->
+              <!-- <input type="text"
+                id="userFormEmail"
+                class="form-control"
+                placeholder="Type your Email@"
+                v-model="components.email"
+                required
+              > -->
             </div>
             <!-- FIM DE BLOCO: New User - Email -->
 
             <!-- INICIO DE BLOCO: New User - Birth -->
             <div class="form-group">
-              <label class="font-weight-bold">User Birth</label>
-              <input type="text" class="form-control" placeholder="YYYY/MM/DD">
+              <!-- <label htmlFor="userFormBirth" class="font-weight-bold">User Birth</label> -->
+              <!-- <input type="text"
+                id="userFormBirth"
+                class="form-control"
+                placeholder="YYYY/MM/DD"
+                v-model="components.birth"
+                required
+              > -->
             </div>
             <!-- FIM DE BLOCO: New User - Birth -->
 
@@ -39,10 +58,20 @@
 
 <script>
 export default {
+  components: {
+    name: 'CreateNewUser',
+  },
   data() {
     return {
-
+      usuario: {
+        name: '',
+        email: '',
+        birth: '',
+      },
     };
+  },
+  methods: {
+    handleSubmitForm() {},
   },
 };
 </script>
