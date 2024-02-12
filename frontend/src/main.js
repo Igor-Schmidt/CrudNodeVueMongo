8bd49d89e4fba9067224ac6399be710fc7f0cabb
IZS <igorschmidt52@gmail.com>
2024-02-12 11:18:50 -0300

    Arrumado use do Sweetalert2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,20 @@
-import VueSweetalert2 from 'vue-sweetalert2';
-
 import { createApp } from 'vue';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
 
 import 'sweetalert2/dist/sweetalert2.min.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'nprogress/nprogress.css';
 
-createApp(App).use(router, VueSweetalert2).mount('#app');
+// createApp(App).use(router, Swal).mount('#app');
+
+// -------------------------------------------------
+const app = createApp(App);
+
+app.use(router);
+
+app.use(VueSweetalert2);
+
+app.mount('#app');
