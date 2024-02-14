e6ccc5dd05af34057fedcef3c225849b9d767f8c
IZS <igorschmidt52@gmail.com>
2024-02-14 17:37:56 -0300

    Testando Lificycle Hooks

--- frontend/src/components/tests/testeLifecycleHooks.vue
+++ frontend/src/components/tests/testeLifecycleHooks.vue
@@ -1,14 +1,64 @@
-<script setup>
-import { ref, onUpdated } from 'vue';
+<template>
+  <!-- Teste de gatilho onMounted -->
+  <!-- INICIO TESTE onMOUNTED -->
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+  <!-- FIM TESTE onMOUNTED -->
 
-const count = ref(0);
+  <!-- Teste de gatilho onUpdated -->
+  <!-- INICIO TESTE onUPDATED -->
+  
+  <!-- <br>
+  <p>clicks: {{ count }}</p>
+  <button class="btn btn-primary" id="count" @click="count++">+ um</button> -->
 
-onUpdated(() => {
-  // o conteúdo de texto deve ser o mesmo que o `count.value` atual
-  console.log(document.getElementById('count').textContent);
-});
+  <!-- FIM TESTE onUPDATED -->
+</template>
+
+
+<script>
+
+// import { ref, onUpdated } from 'vue';
+
+// const count = ref(0);
+
+// onUpdated(() => {
+//   // o conteúdo de texto deve ser o mesmo que o `count.value` atual
+//   console.log(document.getElementById('count').textContent);
+// });
 </script>
 
+
+
 <template>
-  <button id="count" @click="count++">{{ count }}</button>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
 </template>
+
+<!-- <script>
+import { onMounted, ref } from 'vue';
+
+export default {
+  setup() {
+    // Definindo uma variável reativa para armazenar a mensagem
+    const message = ref('');
+
+    // Definindo a lógica a ser executada após o componente ser montado
+    onMounted(() => {
+      // Simulando uma operação assíncrona
+      setTimeout(() => {
+        // Atualizando a mensagem após 2 segundos
+        message.value = 'Componente montado!';
+      }, 2000);
+    });
+
+    // Retornando a variável reativa e/ou outras propriedades
+    return {
+      message
+    };
+  }
+};
+</script> -->
+
